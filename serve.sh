3172d4413c8f80758fce3eba9649ab44492080ed
Wille Marcel <wille@wille.blog.br>
2014-05-12 22:43:17 -0300

    forking to osm

--- serve.sh
+++ serve.sh
@@ -19,4 +19,4 @@ else
   open=xdg-open
 fi
 
-$open http://localhost:$port/template.html && python -m SimpleHTTPServer $port;
+$open http://localhost:$port/osm.html && python2 -m SimpleHTTPServer $port;
